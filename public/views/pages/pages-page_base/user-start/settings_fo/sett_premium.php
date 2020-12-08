74caabd9ba54d1e15a4545246cf38feef5b9add3
Edgar-ae <mantoplonia@gmail.com>
2020-12-08 17:15:49 -0500

    design of settings premium finished and function to 50%

---
+++ public/views/pages/pages-page_base/user-start/settings_fo/sett_premium.php
@@ -0,0 +1,304 @@
+<link rel="stylesheet" href="/public/css/style-settings_fo_premium______.css">
+<link rel="stylesheet" href="/public/css/spectrum_.css">
+
+<div class="container_sett_premium">
+    <div class="header"><img src="/public/svg/premium_icon.svg" alt=""></div>
+    <div class="line"></div>
+    <div class="container_base">
+        <div class="container_1">
+            
+            <div class="space_view">
+                <div id="view" class="view" >
+                    <div id="screen_01" class="screen_01 fondo">
+                        <div class="es00 line">
+                            <div class="es01 d-op-conteinn">
+                                <div class="es02 op-conteinn"><img src="/public/img/premium/v5.png" alt=""></div>
+                                <img src="" alt="">
+                            </div>
+                            <div class="es03">
+                                <div class="es04 conteinn">
+                                    <div class="es04_01">
+                                    <div class="es04_02 buttons"><p>Login</p></div>
+                                    <div class="es04_02 buttons"><p>Register</p></div>
+                                    </div>
+                                </div>
+                                <div class="es05">
+                                    <img src="/public/img/img-fo.jpg" alt="">
+                                    <div class="es06"></div>
+                                    <div class="es07">
+                                        <div class="es08"></div>
+                                        <div class="es09 join"><p>Acceder a la F.O</p></div>
+                                    </div>
+                                </div>
+                                <div class="es10 op-conteinn">
+                                    <div class="es11"></div>
+                                    <div class="es12">
+                                        <div class="es13"></div>
+                                        <div class="es13"></div>
+                                        <div class="es13"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="screen_02" class="screen_02 fondo">
+                        <div class="mo00 line">
+                            <div class="mo01 conteinn">
+                                <div class="mo01_01"></div>
+                                <div class="mo01_02">
+                                        <div class="mo01_02_01 buttons"><p>Login</p></div>
+                                        <div class="mo01_02_01 buttons"><p>Register</p></div>
+                                </div>
+                            </div>
+                            <div class="mo02">
+                                <img class="mo02_1" src="/public/img/img-fo.jpg" alt="">
+                                <img class="mo02_2" src="/public/img/img-fo.jpg" alt="">
+                            </div>
+                            <div class="mo03">
+                            </div>
+                            <div class="mo04 es-op-conteinn">
+                                <div class="mo04_1"></div>
+                                <div class="mo04_2 join"><p>Acceder a la F.O</p></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="controller_bottons">
+                <div id="btn_r_01" class="btn btn_color"><img src="/public/svg/premium/i05.svg" alt=""></div>
+                <div id="btn_r_02" class="btn btn_color"><img src="/public/svg/premium/i06.svg" alt=""></div>
+                <div id="btn_r_03" class="btn btn_color"><img src="/public/svg/premium/i07.svg" alt=""></div>
+            </div>
+        </div>
+        <div id="id_cnt_2" class="container_2">
+            <div id="id_btn_design_buttons" class="secction">
+                <div class="icon_name">
+                    <img src="/public/svg/premium/i02.svg" alt="">
+                    <samp>Diseño de botones</samp>
+                </div>
+                <img class="re" src="/public/svg/premium/i01.svg" alt="">
+            </div>
+            <div id="id_btn_bookstore" class="secction">
+                <div class="icon_name">
+                    <img src="/public/svg/premium/i03.svg" alt="">
+                    <samp>Biblioteca</samp>
+                </div>
+                <img class="re" src="/public/svg/premium/i01.svg" alt="">
+            </div>
+            <!--  -->
+            <div class="secction">
+                <div class="icon_name">
+                    <img src="/public/svg/premium/i04.svg" alt="">
+                    <samp>Color fondo</samp>
+                </div>
+                <div class="nn">
+                <input type='text' class="colors c1" />
+                </div>
+            </div>
+            <!--  -->
+            <div class="secction">
+                <div class="icon_name">
+                    <img src="/public/svg/premium/i04.svg" alt="">
+                    <samp>Color de botones</samp>
+                </div>
+                <div class="nn">
+                <input type='text' class="colors c2" />
+                </div>
+            </div>
+            <!--  -->
+            <div class="secction">
+                <div class="icon_name">
+                    <img src="/public/svg/premium/i04.svg" alt="">
+                    <samp>Color Linea central</samp>
+                </div>
+                <div class="nn">
+                <input type='text' class="colors c3" />
+                </div>
+            </div>
+            <!--  -->
+            <div class="secction">
+                <div class="icon_name">
+                    <img src="/public/svg/premium/i04.svg" alt="">
+                    <samp>Color Containers</samp>
+                </div>
+                <div class="nn">
+                <input type='text' class="colors c4" />
+                </div>
+            </div>
+            <!--  -->
+            <div class="secction">
+                <div class="icon_name">
+                    <img src="/public/svg/premium/i04.svg" alt="">
+                    <samp>Color btn unirse</samp>
+                </div>
+                <div class="nn">
+                <input type='text' class="colors c5" />
+                </div>
+            </div>
+            <div class="btn_acept_cancel">
+                <button id="btn_acept">Aceptar</button>
+                <button id="btn_cancel">Cancelar</button>
+            </div>
+        </div>
+        <!-- Container diseño de botones -->
+        <div id="id_cont_2_design_btn" class="container_2_design_buttons no">
+            <div class="cont_title">
+                <img class="design_btn_return" src="public/svg/premium/chevron-left-solid.svg" alt="">
+                <p>Diseño de botones</p>
+                <img class="check design_btn_return" src="/public/svg/premium/check-circle-regular.svg" alt="">
+            </div>
+            <div class="cont_cont">
+
+            </div>
+        </div>
+        <!-- Container Biblioteca -->
+        <div id="id_cont_2_bookstore" class="container_2_boockstore no">
+        <div class="cont_title">
+                <img class="bookstore_return" src="public/svg/premium/chevron-left-solid.svg" alt="">
+                <p>Libreria</p>
+                <img class="check bookstore_return" src="/public/svg/premium/check-circle-regular.svg" alt="">
+            </div>
+            <div class="cont_cont">
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    $('#btn_r_03').css('opacity','0.5');
+   
+    $('#btn_r_02').css('background','#fdc20041');
+
+    $('#btn_r_01').click(function(){
+        $('#btn_r_02').css('background','#313131');
+        $('#btn_r_01').css('background','#fdc20041');
+        $('#btn_r_03').css('opacity','0.5');
+        only_one_screen();
+    });
+    $('#btn_r_02').click(function(){
+        $('#btn_r_01').css('background','#313131');
+        $('#btn_r_02').css('background','#fdc20041');
+        $('#btn_r_03').css('opacity','0.5');
+        only_two_screen();
+    });
+    $('#btn_r_03').click(function(){
+        /* empty */
+    });
+
+    $('#id_btn_design_buttons').click(function(){
+        $('#id_cnt_2').addClass('no');
+        $('#id_cont_2_design_btn').removeClass('no');
+    });
+    $('.design_btn_return').click(function(){
+        $('#id_cnt_2').removeClass('no');
+        $('#id_cont_2_design_btn').addClass('no');
+    });
+
+
+    $('#id_btn_bookstore').click(function(){
+        $('#id_cnt_2').addClass('no');
+        $('#id_cont_2_bookstore').removeClass('no');
+    });
+    $('.bookstore_return').click(function(){
+        $('#id_cnt_2').removeClass('no');
+        $('#id_cont_2_bookstore').addClass('no');
+    });
+
+
+    function only_one_screen(){
+        $('#screen_02').addClass('no');
+        $('#screen_01').removeClass('no');
+        $('#view').css('height','200px');
+        $('#screen_01').css('transform','scale(1.2)');
+    }
+    function only_two_screen(){
+        $('#screen_01').addClass('no');
+        $('#screen_02').removeClass('no');
+        $('#view').css('height','auto');
+        $('#screen_01').css('transform','scale(1)');
+    }
+    function chanel(){
+        alert('chanel');
+    }
+</script>
+
+<script src="/public/js/spectrum_.js"></script>
+<script>
+    $(".c1").spectrum({
+        replacerClassName: 'awesome',
+        containerClassName: 'awesome',
+        chooseText: "Aceptar",
+        showInput: true,
+        showPalette: true,
+        showPalette: true,
+        palette:[['#36393F'],['#A4DF4A'],['#5693DB']],
+    color: '#45A1E5',
+    change: function(color){
+        $('.fondo').css('background-color',color.toHexString());
+    }
+    });
+    $(".c2").spectrum({
+        replacerClassName: 'awesome',
+        containerClassName: 'awesome',
+        chooseText: "Aceptar",
+        showInput: true,
+        showPalette: true,
+        showPalette: true,
+        palette:[['#36393F'],['#A4DF4A'],['#5693DB']],
+    color: "#45A1E5",
+    change: function(color){
+        $('.buttons').css('background-color',color.toHexString());
+    }
+    });
+    $(".c3").spectrum({
+        replacerClassName: 'awesome',
+        containerClassName: 'awesome',
+        chooseText: "Aceptar",
+        showInput: true,
+        showPalette: true,
+        showPalette: true,
+        palette:[['#36393F'],['#A4DF4A'],['#5693DB']],
+    color: '#fff',
+    change: function(color){
+        $('.line').css('background','linear-gradient(to right,#ffffff00, '+color.toHexString()+'35, #ffffff00)');
+        
+    }
+    });
+    $(".c4").spectrum({
+        replacerClassName: 'awesome',
+        containerClassName: 'awesome',
+        chooseText: "Aceptar",
+        showInput: true,
+        showPalette: true,
+        showPalette: true,
+        palette:[['#36393F'],['#A4DF4A'],['#5693DB']],
+    color: '#36393F',
+    change: function(color){
+        $('.conteinn').css('background-color',color.toHexString());
+        $('.op-conteinn').css('background-color',color.toHexString()+'98');
+        $('.d-op-conteinn').css('background-color',color.toHexString()+'b4');
+        $('.es-op-conteinn').css('background-color',color.toHexString()+'27');
+    }
+    });
+    $(".c5").spectrum({
+        replacerClassName: 'awesome',
+        containerClassName: 'awesome',
+        chooseText: "Aceptar",
+        showInput: true,
+        showPalette: true,
+        showPalette: true,
+        palette:[['#36393F'],['#A4DF4A'],['#5693DB']],
+    color: '#8EDF4A',
+    change: function(color){
+        $('.join').css('background-color',color.toHexString());
+    }
+    });
+</script>
+<style>
+    .awesome{
+    background: #434544;
+    border: none;
+    margin: auto;
+    }
+</style>
--- public/views/pages/pages-page_base/user-start/settings_fo/star_settings_fo.php
+++ public/views/pages/pages-page_base/user-start/settings_fo/star_settings_fo.php
@@ -22,6 +22,7 @@ require_once(URL_PROJECT.'/app/controller/ctr_querys_user.php');
                <li class="<?php if(isset($_GET['edit']))echo'active';?>"><a href="/?settings_fo=set&edit=set">Editar</a></li>
                <li class="<?php if(isset($_GET['photo']))echo'active';?>"><a href="/?settings_fo=set&photo=set">Fotos</a></li>
                <li class="<?php if(isset($_GET['rules']))echo'active';?>"><a href="/?settings_fo=set&rules=set">Reglas</a></li>
+               <li class="<?php if(isset($_GET['sett_premium']))echo'active';?>"><a href="/?settings_fo=set&sett_premium=set" style="color:#FDC300">Premium</a></li>
            <?php endif;?>
            </ul> 
         </div>
@@ -32,6 +33,7 @@ require_once(URL_PROJECT.'/app/controller/ctr_querys_user.php');
             else if(isset($_GET['fo']))include'fo.php';
             else if(isset($_GET['photo']))include'photo.php';
             else if(isset($_GET['rules']))include'rules.php';
+            else if(isset($_GET['sett_premium']))include'sett_premium.php';
             ?>
         </div>
     </div>
